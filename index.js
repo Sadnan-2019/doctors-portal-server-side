731d67f93e57fe4a64e3ee08c662209a96eb8909
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-05-22 02:58:13 +0600

    add appoin available slot

--- index.js
+++ index.js
@@ -26,23 +26,64 @@ async function run() {
     const serviceCollection = client
       .db("doctors_portal")
       .collection("services");
+    const bookingCollection = client
+      .db("doctors_portal")
+      .collection("bookings");
 
+    app.get("/service", async (rew, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-      app.get("/service",async(rew,res)=>{
-          const query={};
-          const cursor = serviceCollection.find(query);
-          const result = await cursor.toArray();
-          res.send(result)
+    app.get("/avaiable", async (req, res) => {
+      const date = req.query.date;
+      //step 1 get all the service
 
+      const services = await serviceCollection.find().toArray();
+      //step 2 get all the booked service of that day
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+      //step 3 create foreach for get single service
 
-})
+      services.forEach((service) => {
+        // step 4 find booking from match service
+        const bookingsMatch = bookings.filter(
+          (booked) =>
+            // step 5 filtering for sigleobject matching both collection and passing a array of object
+            booked.treatment === service.name
+        );
+        //step 6 find ["","",""]
+        const currentBooked = bookingsMatch.map((Matchbook) => Matchbook.slot);
+        // step 7 make out available service
+        const avaiable = service.slots.filter(
+          (slot) => !currentBooked.includes(slot)
+        );
+        service.slots = avaiable;
+      });
+
+      res.send(services);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patientsEmail: booking.patientsEmail,
+      };
+      const exits = await bookingCollection.findOne(query);
+      if (exits) {
+        return res.send({ success: false, booking: exits });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    });
   } finally {
   }
 }
 run().catch(console.dir);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello Doctors Portal World!");
